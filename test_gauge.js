d78be36d43c426583396e31785a511b77db81f4d
8to <otooleat@ornl.gov>
2024-09-07 22:53:14 -0400

    Added scaling to icon.

--- test_gauge.js
+++ test_gauge.js
@@ -1,13 +1,33 @@
 L.GaugeMarker = L.Marker.extend({
     initialize: function (latlng, options) {
         L.Marker.prototype.initialize.call(this, latlng, options);
+        const width = options.icon.options.iconSize[0];
+        const height = options.icon.options.iconSize[1];
+        options.icon.options.html.setAttribute("width", width);
+        options.icon.options.html.setAttribute("height", height);
     },
+
     update_dial: function (degree = 0) {
-        var dial = this._icon.querySelector("svg").querySelector("#dial");
+        var dial = this._icon.querySelector("#dial");
         dial.setAttribute("transform", `rotate(${degree} 115.87487 103.9918)`);
     }
 });
 
 L.gaugeMarker = function (latlng, options) {
-    return new L.GaugeMarker(latlng, options);
+    return new this.GaugeMarker(latlng, options);
 };
+
+function loadSVGSync(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+    if (xhr.status === 200) {
+        const textResponse = xhr.responseText;
+        const parser = new DOMParser();
+        const svgDoc = parser.parseFromString(textResponse, "image/svg+xml");
+        const svgElement = svgDoc.documentElement;
+        return svgElement;
+    } else {
+        throw new Error(`Failed to load SVG: ${xhr.status}`);
+    }
+}
