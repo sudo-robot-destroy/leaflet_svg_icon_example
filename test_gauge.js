f9820bf00f443181b560372d214ab83b9245c8ec
8to <otooleat@ornl.gov>
2024-09-07 20:01:45 -0400

    Load the svg sycn to make it easier.

--- test_gauge.js
+++ test_gauge.js
@@ -11,8 +11,3 @@ L.GaugeMarker = L.Marker.extend({
 L.gaugeMarker = function (latlng, options) {
     return new L.GaugeMarker(latlng, options);
 };
-// function update_dial(degree=0) {
-//     var dial = document.querySelector('.my-icon svg').querySelector("#dial");
-//     var d = dial.getAttribute("d");
-//     dial.setAttribute("transform", `rotate(${degree} 115.87487 103.9918)`);
-// };
